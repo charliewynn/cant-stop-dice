cf695c19f683fd2d0cfaadadd84e19c712fa4341
Charlie Wynn <github@cwynn.com>
2025-07-16 10:36:00 -0500

    moves dice to separate component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Die } from "./dice/die";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Die],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/dice/die.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'die',
+  templateUrl: './die.html',
+  styleUrl: './die.scss'
+})
+export class Die {
+}
